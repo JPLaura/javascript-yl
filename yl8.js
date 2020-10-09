976f1c5efa7b2c3b4aa002b784ce5210e1828a6c
JPLaura <jlaura@K206-18.ametikool.local>
2020-10-09 10:50:16 +0300

    upd8

---
+++ yl8.js
@@ -0,0 +1,11 @@
+const value = 109
+
+if (value%4 == 0) {
+    console.log("On liigaasta")
+}
+else if (value%100 != 0) {
+    console.log("Ei ole liigaasta")
+}
+else {
+    console.log("what")
+}
---
+++ yl9.js
@@ -0,0 +1,4 @@
+const side1 = 36
+const side2 = 65
+const side3 = 2
+
