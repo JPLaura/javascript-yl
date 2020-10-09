9212c2808dd0e9358eb8a37ba8c663fab8a8afa6
JPLaura <jlaura@K206-18.ametikool.local>
2020-10-09 11:36:03 +0300

    upd8

--- yl9.js
+++ yl9.js
@@ -1,4 +1,25 @@
-const side1 = 36
-const side2 = 65
-const side3 = 2
+const side1 = 24
+const side2 = 45
+const side3 = 56
 
+let sides = sides.sort([side1, side2, side3])
+let added = sides[1] + sides[0]
+
+if (sorted[2] > added) {
+    console.log("Yes")
+}
+else {
+    console.log("Oops")
+}
+
+
+
+if (side1 == side2 == side3) {
+    console.log("Võrdkülgne")
+}
+    else if (side1 != side2 == side3) {
+        console.log("Võrdhaarne")
+}
+    else {
+        console.log("no")
+}
